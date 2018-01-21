8c4fb8045d45f02c7d9fc954f858054ef9def5b5
jsjjbuckley <jsjjbuckley@ufl.edu>
2018-01-21 03:16:10 -0500

    Board and Piece Rules

--- Assets/Checkers.cs
+++ Assets/Checkers.cs
@@ -4,13 +4,200 @@ using UnityEngine;
 
 public class Checkers : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Piece[,] pieces = new Piece[8, 8];
+    public GameObject redCheckerPrefab;
+    public GameObject blackCheckerPrefab;
+
+    private bool isRedTurn;
+    private bool isRed;
+
+    private Vector3 boardOffset = new Vector3(-4.0f, 0, -4.0f);
+    private Vector3 pieceOffset = new Vector3(0.5f, 0, 0.5f);
+
+    private Vector2 mouseOver;
+    private Vector2 startDrag;
+    private Vector2 endDrag;
+    private Piece selectedPiece;
+
+    private void Start()
+    {
+        GenerateBoard();
+    }
+
+    private void Update()
+    {
+        UpdateMouseOver();
+
+        //If it is my turn
+        {
+            int x = (int)mouseOver.x;
+            int y = (int)mouseOver.y;
+
+            if (selectedPiece != null)
+            {
+                UpdatePieceDrag(selectedPiece);
+            }
+            //Replace with collision and click of piece and control
+            if (Input.GetMouseButtonDown(0))
+            {
+                SelectPiece(x,y);
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                TryMove((int)startDrag.x, (int)startDrag.y, x, y);
+            }
+        }
+    }
+
+    private void UpdateMouseOver()
+    {
+        //If it's my turn
+        if (!Camera.main)
+        {
+            Debug.Log("Unable to find main camera");
+            return;
+        }
+
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 25.0f, LayerMask.GetMask("ChessPlane")))
+        {
+            mouseOver.x = (int)(hit.point.x - boardOffset.x);
+            mouseOver.y = (int)(hit.point.z - boardOffset.z);
+        }
+        else
+        {
+            mouseOver.x = -1;
+            mouseOver.y = -1;
+        }
+    }
+
+    private void UpdatePieceDrag(Piece p)
+    {
+        if (!Camera.main)
+        {
+            Debug.Log("Unable to find main camera");
+            return;
+        }
+
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 25.0f, LayerMask.GetMask("ChessPlane")))
+        {
+            p.transform.position = hit.point + Vector3.up;
+        }
+    }
+    private void SelectPiece(int x, int y)
+    {
+        //out of bounds
+        if(x < 0 || x >= pieces.Length || y < 0 || y >= pieces.Length)
+        {
+            return;
+        }
+        Piece p = pieces[x, y];
+        if(p != null)
+        {
+            selectedPiece = p;
+            startDrag = mouseOver;
+        }
+    }
+
+    private void TryMove(int x1, int y1, int x2, int y2)
+    {
+        //Multiplayer Support
+        startDrag = new Vector2(x1, y1);
+        endDrag = new Vector2(x2, y2);
+        selectedPiece = pieces[x1, y1];
+        //Out of bounds
+        if(x2 < 0 || x2 >= pieces.Length || y2 < 0 || y2 >= pieces.Length)
+        {
+            if(selectedPiece != null)
+            {
+                MovePiece(selectedPiece, x1, y1);
+            }
+            startDrag = Vector2.zero;
+            selectedPiece = null;
+            return;
+        }
+        //Check if piece selected
+        if(selectedPiece != null)
+        {
+            //If it has not moved
+            if (endDrag == startDrag)
+            {
+                MovePiece(selectedPiece, x1, y1);
+                startDrag = Vector2.zero;
+                selectedPiece = null;
+                return;
+            }
+            //Check if valid move
+            if (selectedPiece.ValidMove(pieces, x1, y1, x2, y2))
+            {
+                //Did we kill anything
+                //If this is a jump
+                if(Mathf.Abs(x2-x1) == 2)
+                {
+                    Piece p = pieces[(x1 + x2) / 2, (y1 + y2) / 2];
+                    if(p != null)
+                    {
+                        pieces[(x1 + x2) / 2, (y1 + y2) / 2] = null;
+                        Destroy(p);
+                    }
+                }
+                pieces[x2, y2] = selectedPiece;
+                pieces[x1, y1] = null;
+                MovePiece(selectedPiece, x2, y2);
+
+                EndTurn();
+            }
+        }
+
+    }
+    private void EndTurn()
+    {
+        selectedPiece = null;
+        startDrag = Vector2.zero;
+        isRedTurn = !isRedTurn;
+        CheckVictory();
+    }
+    private void CheckVictory()
+    {
+
+    }
+    private void GenerateBoard()
+    {
+        //Generate Red team
+        for (int y = 0; y<3; y++)
+        {
+            bool oddRow = (y % 2 == 0);
+            for (int x = 0; x < 8; x += 2)
+            {
+            //Generate our piece
+            GeneratePiece((oddRow) ? x : x+1, y);
+            }
+        }
+
+        //Generate Black team
+        for (int y = 7; y > 4; y--)
+        {
+            bool oddRow = (y % 2 == 0);
+            for (int x = 0; x < 8; x += 2)
+            {
+                //Generate our piece
+                GeneratePiece((oddRow) ? x : x + 1, y);
+            }
+        }
+    }
+    private void GeneratePiece(int x, int y)
+    {
+        bool isPieceRed = (y > 3) ? false : true;
+        GameObject go = Instantiate((isPieceRed) ? redCheckerPrefab : blackCheckerPrefab) as GameObject;
+        go.transform.SetParent(transform);
+        Piece p = go.GetComponent<Piece>();
+        pieces[x, y] = p;
+        MovePiece(p, x, y);
+    }
+
+    private void MovePiece(Piece p, int x, int y)
+    {
+        p.transform.position = (Vector3.right * x) + (Vector3.forward * y) + boardOffset + pieceOffset;
+    }
 }
---
+++ Assets/Piece.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piece : MonoBehaviour {
+    public bool isRed;
+    public bool isKing;
+    public bool ValidMove(Piece[,] board, int x1, int y1, int x2, int y2)
+    {
+        //If moving on top of another piece
+        if (board[x2, y2] != null)
+            return false;
+        int deltaMove = Mathf.Abs(x1 - x2);
+        int deltaMoveY = y2-y1;
+        if(isRed || isKing)
+        {
+
+            if(deltaMove == 1)
+            {
+                if(deltaMoveY == 1)
+                {
+                    return true;
+                }
+            }
+            else if(deltaMove == 2)
+            {
+                if(deltaMoveY == 2)
+                {
+                    Piece p = board[(x1 + x2) / 2, (y1 + y2) / 2];
+                    if (p != null && p.isRed != isRed)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        if (!isRed || isKing)
+        {
+
+            if (deltaMove == 1)
+            {
+                if (deltaMoveY == -1)
+                {
+                    return true;
+                }
+            }
+            else if (deltaMove == 2)
+            {
+                if (deltaMoveY == -2)
+                {
+                    Piece p = board[(x1 + x2) / 2, (y1 + y2) / 2];
+                    if (p != null && p.isRed != isRed)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
